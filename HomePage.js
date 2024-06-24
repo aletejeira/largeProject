52d91df4c93ff1ad32c4045ff9acba5cfc9c43f4
aletej <139593934+aletejeira@users.noreply.github.com>
2024-06-24 17:53:39 -0400

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import HomePageNavbar from '../components/HomePageNavbar';
+
+const HomePage = () =>
+{
+    return(
+    <div>
+        {/* <PageTitle /> */}
+        {/* <HomePageNavbar /> */}
+    </div>
+    );
+};
+
+export default HomePage;
---
+++ LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../components/Login';
+
+const LoginPage = () =>
+{
+    return(
+    <div>
+        {/* <PageTitle /> */}
+        <Login />
+    </div>
+    );
+};
+
+export default LoginPage;
